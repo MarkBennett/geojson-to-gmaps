d01f4dfdc1ad23ff5a23b508fcb1752e44d31b32
Mark Bennett <mark@burmis.ca>
2013-09-18 16:25:03 -0600

    Create Google Map Polyline from LineString.
    It still needs to be shown on the map.

--- spec/GeojsonToGmapsSpec.js
+++ spec/GeojsonToGmapsSpec.js
@@ -1,5 +1,23 @@
 describe("GeojsonToGmaps()", function() {
-    it("adds a GeoJSON LineString to the Google Map");
+    it("adds a GeoJSON LineString to the Google Map", function() {
+        var line_string = {
+            "type": "LineString",
+            "coordinates": [
+                [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+            ]
+        };
+
+        window.google = {};
+        window.google.maps = jasmine.createSpyObj("maps", ["Polyline"]);
+
+        GeojsonToGmaps(line_string);
+
+        expect(window.google.maps.Polyline).toHaveBeenCalledWith({
+            path: [
+                [0.0, 102.0], [1.0, 103.0], [0.0, 104.0], [1.0, 105.0]
+            ]
+        });
+    });
     it("adds a GeoJSON Feature to the Google Map");
     it("adds a GeoJSON FeatureCollection to the Google Map");
 });
--- src/geojson-to-gmaps.js
+++ src/geojson-to-gmaps.js
@@ -1,5 +1,23 @@
 (function() {
-    function GeojsonToGmaps() {}
+
+    function geojson_coordinates_to_gmaps(geojson_coords) {
+        var gmap_coords = new Array(geojson_coords.length);
+        var i;
+
+        for (i=0; i < geojson_coords.length; i++) {
+            gmap_coords[i] = [ geojson_coords[i][1], geojson_coords[i][0] ];
+        }
+
+        return gmap_coords;
+    }
+
+    function GeojsonToGmaps(geojson) {
+        var coordinates = geojson_coordinates_to_gmaps(geojson.coordinates);
+
+        new google.maps.Polyline({
+            path: coordinates
+        });
+    }
 
     GeojsonToGmaps.VERSION = "0.0.0";
 
